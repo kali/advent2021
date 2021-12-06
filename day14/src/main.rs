ae10973ac87a29c872922d8252a870e865e97822
Mathieu Poumeyrol <kali@zoy.org>
2021-12-06 08:41:47 +0100

    copy a template

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input").unwrap();
+    dbg!(run(input.trim()));
+}
+
+fn run(input: &str) -> usize {
+    input.len()
+}
+
+#[test]
+fn t() {
+    assert_eq!(run("input"), 12);
 }
